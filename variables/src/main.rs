02fa8a26dfe7479e9516e607839ada9861bb6e92
Jimmy <dm.a.uvarov@gmail.com>
2022-06-03 10:08:15 -0400

    part 2 done.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,16 @@
+
+const STARTING_MISIILES: i32 = 8;
+const READY_AMMOUNT: i32 = 2;
+
 fn main() {
-    let missiles = 8; 
-    let ready = 2;
+
+    let mut missiles = STARTING_MISIILES; 
+    let ready = READY_AMMOUNT;
+
 
     println!("Fired {} of my {} missiles.", ready,missiles);
+
+    missiles = missiles - ready;
+
+    println!("{} missiles left", missiles);
 }
