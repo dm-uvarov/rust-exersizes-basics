b3ad7890c4ca7f8bc8129176de50a430a7f01256
Jimmy <dm.a.uvarov@gmail.com>
2022-06-12 11:05:45 -0400

    add depency in cargo toml

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -2,8 +2,12 @@
 use sandbox::greet;
 
 // standard library functions colletcions
+// rust std vectoror smthelse
+// rust community crate registry crate.io
 use std::collections::HashMap;
 
+
+// if library name in depencies in cargo.toml we can use it ether directly or as with operator use
 
 fn main() {
     // works directly
