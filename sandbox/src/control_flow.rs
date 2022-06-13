6bfa75434863b060a0e6bc733bc4c949a9b5b4cb
Jimmy <dm.a.uvarov@gmail.com>
2022-06-13 11:21:10 -0400

    preparing for next exersizes

---
+++ sandbox/src/control_flow.rs
@@ -0,0 +1,13 @@
+// ternary 
+// num = if a { b } else { c }; //  = a ? b : c
+// nested
+
+// num = if a { 
+//     if x {y} else {z} 
+//   } else { c }; //
+
+// 'bob
+
+// loop{
+//   break;
+// }
