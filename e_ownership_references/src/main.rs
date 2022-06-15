ac735fde2c3b66e206a634f2b35d4dc761d89b9f
Jimmy <dm.a.uvarov@gmail.com>
2022-06-15 12:23:36 -0400

    e.2 - done.

--- e_ownership_references/src/main.rs
+++ e_ownership_references/src/main.rs
@@ -20,8 +20,8 @@ fn main() {
     // the String if it doesn't already end with "s". Then uncomment and run the code below with
     // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
     //
-    //change(&mut arg);
-    //println!("I have many {}", arg);
+    change(&mut arg);
+    println!("I have many {}", arg);
 
     // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
     // indicating whether or not the String both starts with a "b" AND contains an "a".
@@ -56,4 +56,13 @@ fn inspect(a: &String){
     };
 
     println!("{}", res)
+} // end of inspect()
+
+// fn change()
+
+fn change(s: &mut String){
+    if !s.ends_with("s"){
+    
+    s.push_str("s");
+    };
 }
