388eee8e070c3cddea3c716adad62e5ac1521e20
Jimmy <dm.a.uvarov@gmail.com>
2022-06-15 12:17:44 -0400

    e.1 - done.

--- e_ownership_references/src/main.rs
+++ e_ownership_references/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
     // String reference
     //
-    //inspect(&arg);
+    inspect(&arg);
 
     // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
     // the String if it doesn't already end with "s". Then uncomment and run the code below with
@@ -40,4 +40,20 @@ fn main() {
     // dereferences them and adds them together, and returns the result.
     //
     // println!("1 + 2 = {}, even via references", add(&1, &2));
+}
+
+
+// fn inspect
+
+fn inspect(a: &String){
+
+    let mut res = String::new(); // true -> plural
+
+    if a.ends_with("s"){
+        res.push_str("plural");
+    }else {
+        res.push_str("singular");
+    };
+
+    println!("{}", res)
 }
