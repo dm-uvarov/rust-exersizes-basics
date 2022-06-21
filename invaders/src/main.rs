a1d26a3516e839f42ded415f65505ca64bf6ab58
Jimmy <dm.a.uvarov@gmail.com>
2022-06-20 23:42:59 -0400

    setup alternate screen

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,5 +1,14 @@
-use std::error::Error;
+use std::{
+    error::Error,
+    io,
+};
 use rusty_audio::Audio;
+use crossterm::{
+    cursor::{Hide,Show},
+    terminal::{self,EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+
 
 fn main() -> Result <(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -12,7 +21,19 @@ fn main() -> Result <(), Box<dyn Error>> {
 
     audio.play("startup");
 
+    //terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?; //new screen similar to wim
+    stdout.execute(Hide)?; // no cursor
+
+    println!("alternate screen");
+
     // cleanup 
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
     Ok(())  // return 
 }
